afd0507fb1ea6a4047666bcf7f2ad0a5fd398d8c
Kutayozer17 <kutayozer04@gmail.com>
2025-04-12 15:14:28 +0300

    Create ödev1.sql

---
+++ ödev1.sql
@@ -0,0 +1,10 @@
+SELECT * FROM public.film
+ORDER BY film_id ASC 
+
+select * from film where length > 60 and length < 75;
+
+select * from film where rental_rate = 0.99 and (replacement_cost = 12.99 or replacement_cost = 28.99);
+
+select last_name from customer where first_name = 'Mary';
+
+select * from film where length <= 50 and rental_rate not in (2.99, 4.99);
